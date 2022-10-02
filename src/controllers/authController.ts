a823dcb40bcddf696ccfbdd9d81fcd279c6632e6
ruda <ruda.rabello.silva@gmail.com>
2022-10-02 18:52:43 -0300

    fix: import definition on service

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,1 +1,26 @@
-export {};
+import { NextFunction, Request, Response } from 'express';
+
+import * as authService from '../services/authService'
+
+export async function signUp(
+    req: Request,
+    res: Response
+) {
+    const { type, name, e_mail, phone, password } = req.body;
+
+    await authService.createUser({ type, name, e_mail, phone, password });
+
+    res.status(201).send('user registred sucessfully');
+}
+
+// export async function signIn(
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+// ) {
+//     const { e_mail, password } = req.body;
+
+//     const token = await authService.signIn({ e_mail, password });
+
+//     res.status(200).send({ token });
+// }
