04518574fbcb8d2e3e5cadfc2461e8f2950a74b3
ruda <ruda.rabello.silva@gmail.com>
2022-10-02 18:50:32 -0300

    refactor: authrepository

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { signIn, signUp } from "../controllers/authController";
+import { signUp } from "../controllers/authController";
 import { validateSchema } from "../middlewares/validateSchema";
 import { signInSchema, signUpSchema } from "../schemas/authSchemas";
 
 export const authRouter = Router();
 
 authRouter.post('/sign-up', validateSchema(signUpSchema), signUp);
-authRouter.post('/sign-in', validateSchema(signInSchema), signIn);
+//authRouter.post('/sign-in', validateSchema(signInSchema), signIn);
