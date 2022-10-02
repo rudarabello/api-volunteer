c7564df2ebce6b8afb8937205468552465fcfb5b
ruda <ruda.rabello.silva@gmail.com>
2022-10-02 17:53:43 -0300

    feat: add router main

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,1 +1,9 @@
-export {};
+import { Router } from "express";
+import { signIn, signUp } from "../controllers/authController";
+import { validateSchema } from "../middlewares/validateSchema";
+import { signInSchema, signUpSchema } from "../schemas/authSchemas";
+
+export const authRouter = Router();
+
+authRouter.post('/sign-up', validateSchema(signUpSchema), signUp);
+authRouter.post('/sign-in', validateSchema(signInSchema), signIn);
