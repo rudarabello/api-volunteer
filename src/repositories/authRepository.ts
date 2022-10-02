3ad1b3c988ef3417af24599ef87141450dfbbc86
ruda <ruda.rabello.silva@gmail.com>
2022-10-02 15:01:12 -0300

    feat: add auth repo

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,1 +1,23 @@
-export {};
+import prisma from "../database";
+
+export async function register(
+    type: string,
+    name: string,
+    e_mail: string,
+    phone: number,
+    password: string,
+) {
+    await prisma.users.create({
+        data: {
+            type,
+            name,
+            e_mail,
+            phone,
+            password,
+        }
+    });
+};
+
+export async function findUserByEmail(e_mail: string) {
+    return await prisma.users.findUnique({ where: { e_mail } });
+};
