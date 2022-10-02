14dcea086c37d0dad1877e83a89167b8e72f357f
ruda <ruda.rabello.silva@gmail.com>
2022-10-02 17:53:04 -0300

    refactor: app.use router

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
-import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
-//import recommendationRouter from "./routers/recommendationRouter.js";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
+import router from "./routers/router";
 //import e2eRouter from "./routers/e2eRouter.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-//app.use("/recommendations", recommendationRouter);
+app.use(router);
 // if (process.env.NODE_ENV === "test") {
 //     app.use("/e2e", e2eRouter);
 // }
