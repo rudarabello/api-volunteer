a2206607b59a3395e62ae6a3ccbdb6ef02a81575
ruda <ruda.rabello.silva@gmail.com>
2022-10-05 20:17:25 -0300

    feat: add login user service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,4 @@
-import { encrypt, verifyPassword } from "../utils/jwtUtils";
+import { encrypt, verifyPassword, generateUserToken } from "../utils/jwtUtils";
 import * as userTypes from '../types/userTypes'
 import * as authRepository from '../repositories/authRepository';
 import * as errorUtils from '../utils/errorUtils';
@@ -6,32 +6,23 @@ import * as errorUtils from '../utils/errorUtils';
 
 export async function createUser(CreateUser: userTypes.CreateUser) {
     const { type, name, e_mail, phone, password } = CreateUser;
-
     const user = await authRepository.findUserByEmail(e_mail);
-
-    if (user) throw errorUtils.conflictError('user')
-
+    if (user) throw errorUtils.conflictError('user already register')
     const passwordHash = encrypt(password);
-
     authRepository.insertUser({
         type, name, e_mail, phone, password: passwordHash,
     })
 }
 
-// export async function loginUser(dataUser: authRepository.CreateUser) {
-//     const { email, password } = dataUser;
-
-//     const user = await authRepository.findByEmail(email);
-//     if (!user) throw errorUtils.unauthorizedError('credentials');
-
-//     const matchPassword = await bcrypt.compare(password, user.password);
-//     if (!matchPassword) throw errorUtils.unauthorizedError('credentials');
-
-//     const secretKey = process.env.JWT_SECRET_KEY ?? 'secretKey';
-
-//     const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: '30d' })
-//     return token;
-// }
+export async function loginUser(LoginUser: userTypes.LoginUser) {
+    const { e_mail, password } = LoginUser;
+    const user = await authRepository.findUserByEmail(e_mail);
+    if (!user) throw errorUtils.unauthorizedError('Your credentials does not exists');
+    const matchPassword = await verifyPassword(password, user.password);
+    if (!matchPassword) throw errorUtils.unauthorizedError('Wrong password');
+    const token: string = generateUserToken(user.user_id);
+    return token;
+}
 
 // export async function findUserByEmail(e_mail: string) {
 //     const user = await authRepository.findUserByEmail(e_mail);
