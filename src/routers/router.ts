beff3cba3e720673f988d3947996d0ad16a57650
ruda <ruda.rabello.silva@gmail.com>
2022-10-05 23:44:53 -0300

    feat: add home router on router index

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import { authRouter } from './authRouter';
+import { homeRouter } from './homeRouter';
 
 const router = Router();
 
 router.use(authRouter);
+router.use(homeRouter);
 
 
 export default router;
