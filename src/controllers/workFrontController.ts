01c0439b71a9c2e07d5f138bb64f940a4e321494
ruda <ruda.rabello.silva@gmail.com>
2022-10-08 15:35:55 -0300

    feat: add work front controller

---
+++ src/controllers/workFrontController.ts
@@ -0,0 +1,15 @@
+
+import { Response, Request } from 'express'
+import * as workFrontService from '../services/workFrontService'
+import * as homeTypes from '../types/homeTypes';
+
+export const postWorkFrontCreate = async (req: Request, res: Response) => {
+    const { work_front_name } = req.body;
+    const { userId } = res.locals.user_id;
+    const info: homeTypes.Tworkfront = {
+        work_front_name,
+        manager_id: userId
+    }
+    await workFrontService.createWorkFront(info);
+    res.status(201).send('Work front registred sucessfully');
+}
