7b36687325822ca5cde55f43406ab0572dfe3d13
ruda <ruda.rabello.silva@gmail.com>
2022-10-08 15:36:42 -0300

    feat: add route do creat workfront

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import { authRouter } from './authRouter';
 import { homeRouter } from './homeRouter';
+import { workFrontRouter } from './workFrontRouter';
 
 const router = Router();
 
 router.use(authRouter);
 router.use(homeRouter);
+router.use(workFrontRouter);
 
 
 export default router;
