eba6c7cb61303b0db26964f7e71b9598a409c835
ruda <ruda.rabello.silva@gmail.com>
2022-10-09 19:33:44 -0300

    fix: homeRepository callBack

--- src/services/homeService.ts
+++ src/services/homeService.ts
@@ -1,5 +1,5 @@
 import { schedule } from '@prisma/client';
-import *as homeRepository from '../repositories/homeRepository';
+import * as homeRepository from '../repositories/homeRepository';
 
 export async function findAllSchedule(user_volunteer_id: number): Promise<schedule[] | []> {
     const schedule = await homeRepository.findAllSchedule(user_volunteer_id);
