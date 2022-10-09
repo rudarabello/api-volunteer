fbf7d631b02e3809d0e0a41fabcfae75e11da714
ruda <ruda.rabello.silva@gmail.com>
2022-10-09 19:38:11 -0300

    feat: add service and controller to createWF

--- src/repositories/workFrontRepository.ts
+++ src/repositories/workFrontRepository.ts
@@ -1,8 +1,26 @@
 import prisma from "../database";
 import { Tworkfront } from '../types/homeTypes';
+import { UserId } from '../types/userTypes';
 
 export async function insertWorkFront(info: Tworkfront) {
     return await prisma.work_front.create({
         data: info
     });
 };
+export async function findByWorkFrontName(info: Tworkfront) {
+    const { work_front_name } = info;
+    return await prisma.work_front.findFirst({
+        where: { work_front_name }
+    })
+}
+export async function findbyUserId(info: UserId) {
+    const { user_id } = info;
+    const type = "admim";
+    return await prisma.users.findFirst({
+        where: {
+            user_id,
+            type
+        }
+    });
+};
+
--- src/services/workFrontService.ts
+++ src/services/workFrontService.ts
@@ -1,11 +1,16 @@
 import * as workFrontRepository from '../repositories/workFrontRepository';
 import * as homeTypes from '../types/homeTypes';
+import * as userTypes from '../types/userTypes';
 import * as errorUtils from '../utils/errorUtils';
 
+export async function verifyTypeUser(UserId: userTypes.UserId) {
+    const verifyTypeUser = await workFrontRepository.findbyUserId(UserId);
+    if (!verifyTypeUser) throw errorUtils.conflictError('This user is not admin');
+}
 
 export async function createWorkFront(Tworkfront: homeTypes.Tworkfront) {
-    // verificar se ja tem um work front com esse nome
-    // verificar se o user é do tipo admin       
+    const verifyName = await workFrontRepository.findByWorkFrontName(Tworkfront);
+    if (verifyName) throw errorUtils.conflictError('This work front was already registred');
     const inserted = await workFrontRepository.insertWorkFront(Tworkfront);
     if (!inserted) throw errorUtils.conflictError('Conflict on creation of work front');
 }
