366794ebb352e7b081a250273ecd2b321dd8d0c4
ruda <ruda.rabello.silva@gmail.com>
2022-10-10 14:31:56 -0300

    feat: add router to confirm schedule

--- src/routers/homeRouter.ts
+++ src/routers/homeRouter.ts
@@ -6,6 +6,7 @@ import { validateToken } from "../middlewares/validateToken";
 
 export const homeRouter = Router();
 
-homeRouter.get('/home', validateToken, homeController.getWorkFrontController);
+homeRouter.get('/home', validateToken, homeController.getAllSchedule);
 homeRouter.get('/home/mangers', validateToken, homeController.getNameManagers);
 homeRouter.get('/home/volunteers', validateToken, homeController.getNameVolunteers);
+homeRouter.post('/home/confirm', validateToken, homeController.postConfirmSchedule);
