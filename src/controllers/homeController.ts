ff1eeaa2a1ca40e9b6070467d8d9cbf6ee4556d2
ruda <ruda.rabello.silva@gmail.com>
2022-10-10 14:33:07 -0300

    feat: add controller to confirm schedule

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,7 +2,7 @@
 import { Response, Request } from 'express'
 import * as homeService from '../services/homeService'
 
-export const getWorkFrontController = async (req: Request, res: Response) => {
+export const getAllSchedule = async (req: Request, res: Response) => {
     const { userId } = res.locals.user_id;
     const schedule = await homeService.findAllSchedule(userId)
     res.status(201).send(schedule);
@@ -16,4 +16,9 @@ export const getNameManagers = async (req: Request, res: Response) => {
 export const getNameVolunteers = async (req: Request, res: Response) => {
     const nameOfVolunteers = await homeService.getNameOfVolunteers()
     res.status(201).send(nameOfVolunteers);
+}
+export const postConfirmSchedule = async (req: Request, res: Response) => {
+    const { schedule_id } = req.body;
+    await homeService.confirmSchedule(schedule_id);
+    res.status(201).send('Schedule confirmed');
 }
