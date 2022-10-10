1e1c7348263f812777276cb33e736440d2e975b8
ruda <ruda.rabello.silva@gmail.com>
2022-10-10 10:25:34 -0300

    feat: add types to work on schedule

--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -7,3 +7,5 @@ export type LoginUser = Omit<users, "user_id" | "type" | "name" | "phone">;
 export type TypeUser = Omit<users, "user_id" | "name" | "e_mail" | "phone" | "password">;
 
 export type UserId = Omit<users, "type" | "name" | "e_mail" | "phone" | "password">;
+
+export type EmailUser = Omit<users, "user_id" | "type" | "name" | "phone" | "password">;
