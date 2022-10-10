8d6b40c1f086ad8632cd5b3085cd5dad190479cc
ruda <ruda.rabello.silva@gmail.com>
2022-10-10 10:26:08 -0300

    fix: delete findUserByEmail

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -18,3 +18,16 @@ export async function findUserByPhone(phone: string) {
 export const getUserById = async (user_id: number) => {
     return await prisma.users.findUnique({ where: { user_id } });
 };
+
+// export async function findUserIdByEmail(e_mail: string) {
+//     const email = e_mail;
+//     const user_volunteer_id = await prisma.users.findUnique({
+//         where: {
+//             e_mail: email
+//         },
+//         select: {
+//             user_id: true
+//         }
+//     });
+//     return user_volunteer_id
+// };
