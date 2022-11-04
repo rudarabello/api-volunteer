eb780997989f7d55c528b27623d36705ef8a546a
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-04 17:11:25 -0300

    feat: add validateType function

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { signUp, signIn } from "../controllers/authController";
 import { validateSchema } from "../middlewares/validateSchema";
+import { validateType } from "../middlewares/validateType";
 import { signInSchema, signUpSchema } from "../schemas/authSchemas";
 
 export const authRouter = Router();
 
-authRouter.post('/sign-up', validateSchema(signUpSchema), signUp);
+authRouter.post('/sign-up', validateSchema(signUpSchema), validateType, signUp);
 authRouter.post('/sign-in', validateSchema(signInSchema), signIn);
