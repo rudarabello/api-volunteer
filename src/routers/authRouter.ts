80500487037cf722b3e285531028cddcc2df4b53
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-10 16:31:57 -0300

    feat: add sanitize datas

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { signUp, signIn } from "../controllers/authController";
+import { sanitizeDatas } from "../middlewares/dataSanitizationMiddleware";
 import { validateSchema } from "../middlewares/validateSchema";
 import { validateType } from "../middlewares/validateType";
 import { signInSchema, signUpSchema } from "../schemas/authSchemas";
 
 export const authRouter = Router();
 
-authRouter.post('/sign-up', validateSchema(signUpSchema), validateType, signUp);
-authRouter.post('/sign-in', validateSchema(signInSchema), signIn);
+authRouter.post('/sign-up', sanitizeDatas, validateSchema(signUpSchema), validateType, signUp);
+authRouter.post('/sign-in', sanitizeDatas, validateSchema(signInSchema), signIn);
