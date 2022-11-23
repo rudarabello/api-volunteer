716007cbd4a40c5bf8df197b7d62065607aeedfc
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-23 09:23:05 -0300

    fix: change types

--- src/types/homeTypes.ts
+++ src/types/homeTypes.ts
@@ -1,7 +1,7 @@
 import { schedule, work_front } from '@prisma/client';
 
-export type Tschedule = Omit<schedule, "schedule_id" | "work_front_id" | "date_to_work" | "time_to_work" | "invite_accept">;
+export type Tschedule = Omit<schedule, 'schedule_id' | 'work_front_id' | 'date_to_work' | 'time_to_work' | 'invite_accept'>;
 
-export type Tworkfront = Omit<work_front, "work_front_id">;
+export type Tworkfront = Omit<work_front, 'work_front_id'>;
 
-export type TscheduleId = Omit<schedule, "schedule_id">;
+export type TscheduleId = Omit<schedule, 'schedule_id'>;
--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -1,11 +1,11 @@
 import { users } from '@prisma/client';
 
-export type CreateUser = Omit<users, "user_id">;
+export type CreateUser = Omit<users, 'user_id'>;
 
-export type LoginUser = Omit<users, "user_id" | "type" | "name" | "phone">;
+export type LoginUser = Omit<users, 'user_id' | 'type' | 'name' | 'phone'>;
 
-export type TypeUser = Omit<users, "user_id" | "name" | "e_mail" | "phone" | "password">;
+export type TypeUser = Omit<users, 'user_id' | 'name' | 'e_mail' | 'phone' | 'password'>;
 
-export type UserId = Omit<users, "type" | "name" | "e_mail" | "phone" | "password">;
+export type UserId = Omit<users, 'type' | 'name' | 'e_mail' | 'phone' | 'password'>;
 
-export type EmailUser = Omit<users, "user_id" | "type" | "name" | "phone" | "password">;
+export type EmailUser = Omit<users, 'user_id' | 'type' | 'name' | 'phone' | 'password'>;
