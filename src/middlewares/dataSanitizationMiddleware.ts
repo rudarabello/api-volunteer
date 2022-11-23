aacf6054715792b870e11b3c005a983551f0b97b
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-23 09:15:29 -0300

    fix: change parameters of sanitazeDatas

--- src/middlewares/dataSanitizationMiddleware.ts
+++ src/middlewares/dataSanitizationMiddleware.ts
@@ -1,15 +1,13 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 
-export const sanitizeDatas = (schema: any) => {
-  return (req: Request, res: Response, next: NextFunction) => {
+export const sanitizeDatas = (req: Request, res: Response, next: NextFunction) => {
     if (!req.body) next();
 
     Object.keys(req.body).forEach((key) => {
-      if (typeof req.body[key] === 'string') {
-        req.body[key] = req.body[key].replace(/<\/?[^>]+(>|$)/g, "").result.trim();
-      }
+        if (typeof req.body[key] === 'string') {
+            req.body[key] = req.body[key].replace(/<\/?[^>]+(>|$)/g, '').trim();
+        }
     });
 
     next();
-  };
 };
